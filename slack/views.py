1d9c10cf9685d210d73702213811ef9c4e3512dd
Lenny Pratt <lenny.pratt@hireanesquire.com>
2017-06-03 16:41:11 -0400

    Make post csrf exempt

--- slack/views.py
+++ slack/views.py
@@ -3,10 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import HelloSlackAdapter
 
 
+@method_decorator(csrf_exempt, name='dispatch')
 class RollView(View):
     def post(self, request, *args, **kwargs):
         return HelloSlackAdapter().respond_to_hello()
